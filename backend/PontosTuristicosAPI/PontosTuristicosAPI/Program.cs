0cdcc3838af7364c0dc1917f369e4ff802261ece
Gabriel Hosoume <gabriel.hosoume@outlook.com>
2025-01-12 23:32:29 -0300

    feat: implementar versão inicial da API
    Inclui os seguintes endpoints:
    - Ping
    - Operações CRUD para os pontos turísticos

---
+++ backend/PontosTuristicosAPI/PontosTuristicosAPI/Program.cs
@@ -0,0 +1,37 @@
+using PontosTuristicosAPI.Repositories;
+using PontosTuristicosAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddScoped<IPontoTuristicoRepository, PontoTuristicoRepository>();
+builder.Services.AddScoped<IPontoTuristicoService, PontoTuristicoService>();
+ builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
